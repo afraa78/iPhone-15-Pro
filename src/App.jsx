9061fe883d1d8ce835a61167a9597f07b6691efa
afraa78 <afraamoh178@gmail.com>
2026-07-08 17:38:54 +0300

    Added file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero.jsx'
 import Highlights from './components/Highlights.jsx'
 import Model from './components/Model.jsx'
 import Features from './components/Features.jsx'
+import Chip from './components/Chip.jsx'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Highlights/>
       <Model/>
       <Features/>
+      <Chip/>
     </main>
   )
 }
